91037ac123b744d069bc4dd7fd5d61c59974555b
WoodCollector <you@example.com>
2022-11-21 22:12:44 +0100

    fixed deploy

--- deploy/02-deploy-minter.js
+++ deploy/02-deploy-minter.js
@@ -1,10 +1,11 @@
 const { network } = require("hardhat")
-const { developmentChains } = require("../helper-hardhat-config")
+const { networkConfig, developmentChains } = require("../helper-hardhat-config")
 const { verify } = require("../utils/verify")
 
 module.exports = async function ({ getNamedAccounts, deployments }) {
     const { deploy, log } = deployments
     const { deployer } = await getNamedAccounts()
+    const chainId = network.config.chainId
     log("------------------------")
 
     let ethUsdPriceFeedAddress
